d3d7088cb05a63cd2a7ceb7a5caf07958c1df501
Colin <hungrymamamia@gmail.com>
2024-04-07 11:55:59 -0700

    initial commit of slack bot

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
 # This repository will likely not be updated.
 # Please refer to https://replit.com/@cjplusplus/wishbot.
 
-import discord
-from discord.ext import tasks
+import slack
 import os
 import asyncio
 from datetime import datetime
 import pytz
+from pathlib import Path
+from dotenv import load_dotenv
 from keep_alive import keep_alive
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
+client.chat_postMessage(channel="#wish", text="Wishbot is online!")
 
 
-@tasks.loop(seconds=3)
 async def check_time():
   current_time_raw = datetime.now(pytz.timezone("America/Los_Angeles"))
   is_9 = (current_time_raw.strftime("%H")
@@ -26,18 +28,20 @@ async def check_time():
   #is_minute = current_time_raw.strftime("%M") == "11"
   #is_time = is_hour and is_minute
   if is_948:
-    general = client.get_channel(921915887521509418)
-    await general.send("<@&1125686347076751433> wish!")
-    await asyncio.sleep(60)
-  #await check_time()
+    client.chat_postMessage(channel="#wish", text="<!channel> 9:48! Make a wish! ✨⚡")
+    return True
+  return False
 
 
-@client.event
-async def on_ready():
-  print("We have logged in as {0.user}".format(client))
-  await check_time.start()
-
-
+async def main_loop(interval=2):
+  while True:
+    if await check_time():
+      await asyncio.sleep(60) # Pauses bot for a minute to eliminate duplicate wishes
+    else:
+      await asyncio.sleep(interval)
+keep_alive()
+asyncio.run(main_loop())
+"""
 @client.event
 async def on_message(message):
   if message.author == client.user:
@@ -54,4 +58,4 @@ async def on_message(message):
 
 
 keep_alive()
-client.run(os.getenv("TOKEN"))
+client.run(os.getenv("TOKEN"))"""
